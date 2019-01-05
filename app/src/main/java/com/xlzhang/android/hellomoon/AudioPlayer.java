f9aca3f0b84654b51c1bb6b912603324e9eaa13e
xlzhang18 <2807774501@qq.com>
2019-01-05 20:04:32 +0800

    add plalayy/pause and stop listener

---
+++ app/src/main/java/com/xlzhang/android/hellomoon/AudioPlayer.java
@@ -0,0 +1,35 @@
+package com.xlzhang.android.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class AudioPlayer {
+    private MediaPlayer mPlayer;
+
+    public void stop() {
+        if(mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context c){
+        stop();
+        mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stop();
+            }
+        });
+        mPlayer.start();
+    }
+
+    public void pause(){
+        if(mPlayer != null){
+            if(mPlayer.isPlaying())
+                mPlayer.pause();
+            else mPlayer.start();
+        }
+    }
+}
---
+++ app/src/main/java/com/xlzhang/android/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,54 @@
+package com.xlzhang.android.hellomoon;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HelloMoonFragment extends Fragment {
+    private Button mPlayButton;
+    private Button mPauseButton;
+    private Button mStopButton;
+    private AudioPlayer mPlayer = new AudioPlayer();
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
+
+        mPlayButton = v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.play(getActivity());
+            }
+        });
+        mPauseButton = v.findViewById(R.id.hellomoon_pauseButton);
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.pause();
+            }
+        });
+        mStopButton = v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.stop();
+            }
+        });
+
+        return v;
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
+    }
+}
